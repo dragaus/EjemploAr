027ed3022cf429a6cabddea6a2d6b6eebaa8a263
Francisco Cuauhtemoc Castañeda Rovira <31245060+dragaus@users.noreply.github.com>
2024-01-16 17:36:15 -0600

    Upgrade

---
+++ Assets/LenteManager.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+
+public class LenteManager : MonoBehaviour
+{
+    public void SeleccionLentes(int numeroLente)
+    {
+        GameObject cara = FindObjectOfType<ARFace>().gameObject;
+
+        //Encontar Lentes
+        Transform lentesManager = cara.transform.GetChild(1).transform;
+
+        //Desactivariamos todos los lentes
+        for(int i = 0; i < lentesManager.childCount;i++)
+        {
+            //activar el lente seleccionado
+            lentesManager.GetChild(i).gameObject.SetActive(i == numeroLente);
+        }
+    }
+}
